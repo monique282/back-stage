c1b36a83bab68f101d7987027ef2a599acbcdd00
Monique de Souza <moniquejp16@gmail.com>
2025-03-26 11:00:27 -0300

    feat: route for deletion of the area

--- src/controllers/treeController.ts
+++ src/controllers/treeController.ts
@@ -6,8 +6,12 @@ async function treeGet(req: Request, res: Response) {
 
     const user = await treeService.treeGet();
     res.status(httpStatus.OK).send(user);
-}
+};
+
+async function areaDelete(req: Request, res: Response) {
+    
+},
 
 export const treeComtroller = {
-    treeGet
+    treeGet, areaDelete
 };
--- src/routes/treeRoute.ts
+++ src/routes/treeRoute.ts
@@ -1,11 +1,13 @@
 
 import { treeComtroller } from '@/controllers/treeController';
+import { authenticateToken } from '@/middlewares/authenticationTokenMiddleware';
 import { Router } from 'express';
 
 
 const TreeRoute = Router();
 
 TreeRoute.get('/', treeComtroller.treeGet);
+TreeRoute.delete('/delete/:id', authenticateToken, treeComtroller.areaDelete)
 
 
 export { TreeRoute };
