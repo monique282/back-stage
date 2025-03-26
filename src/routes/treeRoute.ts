2d179ee876fb1fcddba69c07b5ce093e9f90953a
Monique de Souza <moniquejp16@gmail.com>
2025-03-26 11:25:14 -0300

    feat: process deletion route

--- src/routes/treeRoute.ts
+++ src/routes/treeRoute.ts
@@ -1,13 +1,15 @@
 
 import { treeComtroller } from '@/controllers/treeController';
 import { authenticateToken } from '@/middlewares/authenticationTokenMiddleware';
+import { checkAdmin } from '@/middlewares/checkAdminMiddleware';
 import { Router } from 'express';
 
 
 const TreeRoute = Router();
 
 TreeRoute.get('/', treeComtroller.treeGet);
-TreeRoute.delete('/delete/:id', authenticateToken, treeComtroller.areaDelete)
+TreeRoute.delete('/deleteArea/:id', authenticateToken, checkAdmin, treeComtroller.areaDelete)
+TreeRoute.delete('/deleteProcess/:id', authenticateToken, checkAdmin,treeComtroller.processDelete)
 
 
 export { TreeRoute };
